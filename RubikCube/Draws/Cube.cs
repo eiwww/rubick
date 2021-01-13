40d12161798831c1286239b069aeadd93c7cf4fd
EIWWW <ketsadaphonev@gmail.com>
2021-01-13 14:21:11 +0700

    rubickk

---
+++ RubikCube/Draws/Cube.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Tao.OpenGl;
+
+namespace RubikCube.Draws
+{
+    class Cube : IDraw
+    {
+        private double size;
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public double Z { get; private set; }
+
+        private FaceCube<Color> faceColors;
+
+        private int AngleX = 0;
+        private int AngleY = 0;
+        private int AngleZ = 0;
+
+
+        public Cube(double size, double X, double Y, double Z)
+        {
+            this.size = size;
+            this.X = X;
+            this.Y = Y;
+            this.Z = Z;
+            this.faceColors = new FaceCube<Color>(Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black);
+        }
+
+        public Cube(double size, double X, double Y, double Z, FaceCube<Color> faceColors)
+        {
+            this.size = size;
+            this.X = X;
+            this.Y = Y;
+            this.Z = Z;
+            this.faceColors = faceColors;
+        }
+
+        public Cube(double size, double X, double Y, double Z, FaceCube<Texture> faceColors)
+        {
+            this.size = size;
+            this.X = X;
+            this.Y = Y;
+            this.Z = Z;
+            //this.faceColors = faceColors;
+        }
+
+        public void Rotate(int AngleAxisX, int AngleAxisY, int AngleAxisZ)
+        {
+            this.AngleX += AngleAxisX;
+            this.AngleY += AngleAxisY;
+            this.AngleZ += AngleAxisZ;
+        }
+
+        public void Draw()
+        {
+
+            Gl.glEnable(Gl.GL_TEXTURE);
+
+            Gl.glPushMatrix();
+            Gl.glRotatef(AngleX, 1, 0, 0);
+            Gl.glRotatef(AngleY, 0, 1, 0);
+            Gl.glRotatef(AngleZ, 0, 0, 1);
+
+            Gl.glBegin(Gl.GL_QUADS);
+
+            //Traseira
+            Gl.glColor3ub(faceColors.Back.R, faceColors.Back.G, faceColors.Back.B);
+            Gl.glVertex3d(this.size + X, this.size + Y, -this.size + Z);
+            Gl.glVertex3d(this.size + X, -this.size + Y, -this.size + Z);
+            Gl.glVertex3d(-this.size + X, -this.size + Y, -this.size + Z);
+            Gl.glVertex3d(-this.size + X, this.size + Y, -this.size + Z);
+
+            //Inferior
+            Gl.glColor3ub(faceColors.Bottom.R, faceColors.Bottom.G, faceColors.Bottom.B);
+            Gl.glVertex3d(-this.size + X, -this.size + Y, -this.size + Z);
+            Gl.glVertex3d(this.size + X, -this.size + Y, -this.size + Z);
+            Gl.glVertex3d(this.size + X, -this.size + Y, this.size + Z);
+            Gl.glVertex3d(-this.size + X, -this.size + Y, this.size + Z);
+
+            //Esquerda
+            Gl.glColor3ub(faceColors.Left.R, faceColors.Left.G, faceColors.Left.B);
+            Gl.glVertex3d(-this.size + X, this.size + Y, -this.size + Z);
+            Gl.glVertex3d(-this.size + X, -this.size + Y, -this.size + Z);
+            Gl.glVertex3d(-this.size + X, -this.size + Y, this.size + Z);
+            Gl.glVertex3d(-this.size + X, this.size + Y, this.size + Z);
+
+            //Direita
+            Gl.glColor3ub(faceColors.Right.R, faceColors.Right.G, faceColors.Right.B);
+            Gl.glVertex3d(this.size + X, this.size + Y, this.size + Z);
+            Gl.glVertex3d(this.size + X, -this.size + Y, this.size + Z);
+            Gl.glVertex3d(this.size + X, -this.size + Y, -this.size + Z);
+            Gl.glVertex3d(this.size + X, this.size + Y, -this.size + Z);
+
+            ///Superior
+            Gl.glColor3ub(faceColors.Top.R, faceColors.Top.G, faceColors.Top.B);
+            Gl.glVertex3d(-this.size + X, this.size + Y, -this.size + Z);
+            Gl.glVertex3d(-this.size + X, this.size + Y, this.size + Z);
+            Gl.glVertex3d(this.size + X, this.size + Y, this.size + Z);
+            Gl.glVertex3d(this.size + X, this.size + Y, -this.size + Z);
+
+            //Frente
+            Gl.glColor3ub(faceColors.Front.R, faceColors.Front.G, faceColors.Front.B);
+            Gl.glVertex3d(-this.size + X, this.size + Y, this.size + Z);
+            Gl.glVertex3d(-this.size + X, -this.size + Y, this.size + Z);
+            Gl.glVertex3d(this.size + X, -this.size + Y, this.size + Z);
+            Gl.glVertex3d(this.size + X, this.size + Y, this.size + Z);
+
+            Gl.glEnd();
+
+            Gl.glPopMatrix();
+        }
+
+        public void Place(RubikCubeMoviment moviment)
+        {
+            this.faceColors.Rotate(moviment);
+            this.Transform(moviment);
+            this.ResetTransforms();
+        }
+
+        private void Transform(RubikCubeMoviment moviment)
+        {
+            if(moviment.Axis == Axis.X)
+            {
+                if(moviment.Spin == Spin.Clockwise) //Up
+                {
+                    //Edges
+                    if(this.Y < 0 && this.Z < 0)
+                    {
+                        this.Z = -this.Z;
+                    }
+                    else if(this.Y < 0 && this.Z > 0)
+                    {
+                        this.Y = -this.Y;
+                    }
+                    else if(this.Y > 0 && this.Z < 0)
+                    {
+                        this.Y = -this.Y;
+                    }
+                    else if(this.Y > 0 && this.Z > 0)
+                    {
+                        this.Z = -this.Z;
+                    }
+
+                    //Middles
+                    else if(this.Y == 0 && this.Z < 0)
+                    {
+                        this.Y = this.Z;
+                        this.Z = 0;
+                    }
+                    else if (this.Y == 0 && this.Z > 0)
+                    {
+                        this.Y = this.Z;
+                        this.Z = 0;
+                    }
+                    else if (this.Z == 0 && this.Y < 0)
+                    {
+                        this.Z = -this.Y;
+                        this.Y = 0;
+                    }
+                    else if (this.Z == 0 && this.Y > 0)
+                    {
+                        this.Z = -this.Y;
+                        this.Y = 0;
+                    }
+                }
+                else //Down
+                {
+                    //Edges
+                    if (this.Y < 0 && this.Z < 0)
+                    {
+                        this.Y = -this.Y;
+                    }
+                    else if (this.Y < 0 && this.Z > 0)
+                    {
+                        this.Z = -this.Z;
+                    }
+                    else if (this.Y > 0 && this.Z < 0)
+                    {
+                        this.Z = -this.Z;
+                    }
+                    else if (this.Y > 0 && this.Z > 0)
+                    {
+                        this.Y = -this.Y;
+                    }
+
+                    //Middles
+                    else if (this.Y == 0 && this.Z < 0)
+                    {
+                        this.Y = -this.Z;
+                        this.Z = 0;
+                    }
+                    else if (this.Y == 0 && this.Z > 0)
+                    {
+                        this.Y = -this.Z;
+                        this.Z = 0;
+                    }
+                    else if (this.Z == 0 && this.Y < 0)
+                    {
+                        this.Z = this.Y;
+                        this.Y = 0;
+                    }
+                    else if (this.Z == 0 && this.Y > 0)
+                    {
+                        this.Z = this.Y;
+                        this.Y = 0;
+                    }
+                }
+            }
+
+            if(moviment.Axis == Axis.Y)
+            {
+                if (moviment.Spin == Spin.Clockwise) //Left
+                {
+                    if (this.X < 0 && this.Z < 0)
+                    {
+                        this.X = -this.X;
+                    }
+                    else if (this.X < 0 && this.Z > 0)
+                    {
+                        this.Z = -this.Z;
+                    }
+                    else if (this.X > 0 && this.Z < 0)
+                    {
+                        this.Z = -this.Z;
+                    }
+                    else if (this.X > 0 && this.Z > 0)
+                    {
+                        this.X = -this.X;
+                    }
+
+                    //Middle
+                    else if(this.X == 0 && this.Z < 0)
+                    {
+                        this.X = -this.Z;
+                        this.Z = 0;
+                    }
+                    else if(this.X == 0 && this.Z > 0)
+                    {
+                        this.X = -this.Z;
+                        this.Z = 0;
+                    }
+                    else if(this.Z == 0 && this.X < 0)
+                    {
+                        this.Z = this.X;
+                        this.X = 0;
+                    }
+                    else if(this.Z == 0 && this.X > 0)
+                    {
+                        this.Z = this.X;
+                        this.X = 0;
+                    }
+                }
+                else
+                {
+                    if (this.X < 0 && this.Z < 0)
+                    {
+                        this.Z = -this.Z;
+                    }
+                    else if (this.X < 0 && this.Z > 0)
+                    {
+                        this.X = -this.X;
+                    }
+                    else if (this.X > 0 && this.Z < 0)
+                    {
+                        this.X = -this.X;
+                    }
+                    else if (this.X > 0 && this.Z > 0)
+                    {
+                        this.Z = -this.Z;
+                    }
+
+                    //Middle
+                    else if (this.X == 0 && this.Z < 0)
+                    {
+                        this.X = this.Z;
+                        this.Z = 0;
+                    }
+                    else if (this.X == 0 && this.Z > 0)
+                    {
+                        this.X = this.Z;
+                        this.Z = 0;
+                    }
+                    else if (this.Z == 0 && this.X < 0)
+                    {
+                        this.Z = -this.X;
+                        this.X = 0;
+                    }
+                    else if (this.Z == 0 && this.X > 0)
+                    {
+                        this.Z = -this.X;
+                        this.X = 0;
+                    }
+                }
+            }
+
+            if(moviment.Axis == Axis.Z)
+            {
+                if(moviment.Spin == Spin.Clockwise)
+                {
+                    if (this.X < 0 && this.Y < 0)
+                    {
+                        this.Y = -this.Y;
+                    }
+                    else if (this.X < 0 && this.Y > 0)
+                    {
+                        this.X = -this.X;
+                    }
+                    else if (this.X > 0 && this.Y < 0)
+                    {
+                        this.X = -this.X;
+                    }
+                    else if (this.X > 0 && this.Y > 0)
+                    {
+                        this.Y = -this.Y;
+                    }
+
+                    //Middle
+                    else if (this.X == 0 && this.Y < 0)
+                    {
+                        this.X = this.Y;
+                        this.Y = 0;
+                    }
+                    else if (this.X == 0 && this.Y > 0)
+                    {
+                        this.X = this.Z;
+                        this.Y = 0;
+                    }
+                    else if (this.Y == 0 && this.X < 0)
+                    {
+                        this.Y = -this.X;
+                        this.X = 0;
+                    }
+                    else if (this.Y == 0 && this.X > 0)
+                    {
+                        this.Y = -this.X;
+                        this.X = 0;
+                    }
+                }
+                else
+                {
+                    if (this.X < 0 && this.Y < 0)
+                    {
+                        this.X = -this.X;
+                    }
+                    else if (this.X < 0 && this.Y > 0)
+                    {
+                        this.Y = -this.Y;
+                    }
+                    else if (this.X > 0 && this.Y < 0)
+                    {
+                        this.Y = -this.Y;
+                    }
+                    else if (this.X > 0 && this.Y > 0)
+                    {
+                        this.X = -this.X;
+                    }
+
+                    //Middle
+                    else if (this.X == 0 && this.Y < 0)
+                    {
+                        this.X = -this.Y;
+                        this.Y = 0;
+                    }
+                    else if (this.X == 0 && this.Y > 0)
+                    {
+                        this.X = -this.Z;
+                        this.Y = 0;
+                    }
+                    else if (this.Y == 0 && this.X < 0)
+                    {
+                        this.Y = this.X;
+                        this.X = 0;
+                    }
+                    else if (this.Y == 0 && this.X > 0)
+                    {
+                        this.Y = this.X;
+                        this.X = 0;
+                    }
+                }
+            }
+            
+            this.ResetTransforms();
+        }
+
+        private void ResetTransforms()
+        {
+            this.AngleX = 0;
+            this.AngleY = 0;
+            this.AngleZ = 0;
+        }
+    }
+}
---
+++ RubikCube/Form1.cs
@@ -0,0 +1,167 @@
+﻿using RubikCube.Draws;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.OpenGl;
+
+namespace RubikCube
+{
+    public partial class RubikCubeForm : Form
+    {
+        System.Media.SoundPlayer mp3 = new System.Media.SoundPlayer();
+        private Timer timer;
+
+        private RubiksCube rubiksCube;
+        private List<Light> lights;
+
+
+        public RubikCubeForm()
+        {
+            InitializeComponent();
+            this.timer = new Timer();
+            timer.Tick += Tick;
+            timer.Enabled = true;
+            timer.Interval = 1;
+            timer.Start();
+
+            mp3.SoundLocation = "wii sound.wav";
+
+            mp3.PlayLooping();
+            
+
+            
+            OpenGlControl.Width = this.Width;
+            OpenGlControl.Height = this.Height;
+            OpenGlControl.InitializeContexts();
+
+            rubiksCube = new RubiksCube();
+            lights = new List<Light>();
+
+            var lightFront = new Light(Gl.GL_LIGHT0, new float[3] { 0.0f, 0.0f, 3f });
+            lights.Add(lightFront);
+            //var lightBack = new Light(Gl.GL_LIGHT0, new float[3] { 0f, 0f, 3f });
+            //lights.Add(lightBack);
+
+
+            this.Initialize();            
+        }
+
+        private void Initialize()
+        {
+
+            Gl.glViewport(0, 0, this.Width, this.Height);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Glu.gluPerspective(45.0f, (double)this.Width / (double)this.Height, 0.01f, 5000.0f);
+            Gl.glEnable(Gl.GL_CULL_FACE);
+            Gl.glCullFace(Gl.GL_BACK);
+
+            Gl.glClearColor(.85f, .85f, .85f, 1);
+            Gl.glClear(Gl.GL_CLEAR);
+        }
+
+        private void Tick(object sender, EventArgs e)
+        {
+            this.OpenGlControl.Invalidate();
+        }
+
+        public void Manipulate(RubikCubeMoviment moviment)
+        {
+            rubiksCube.Manipulate(moviment);
+        }
+        
+        private void OpenGlControl_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D)
+            {
+                rubiksCube.Rotate(0, 5, 0);
+                
+            }
+            else if (e.KeyCode == Keys.A)
+            {
+                rubiksCube.Rotate(0, -5, 0);
+                
+            }
+            else if (e.KeyCode == Keys.W)
+            {
+                rubiksCube.Rotate(-5, 0, 0);
+                
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                rubiksCube.Rotate(5, 0, 0); 
+            }
+
+            else if(e.KeyCode == Keys.B)
+            {
+                foreach(var light in lights)
+                {
+                    light.IsAmbientLightEnabled = !light.IsAmbientLightEnabled;
+                    Debug.WriteLine("State: Ambient: {0}, Diffuse: {1}, Specular: {2} ", light.IsAmbientLightEnabled, light.IsDiffuseLightEnabled, light.IsSpecularLightEnabled);
+                }
+                
+            }
+            else if (e.KeyCode == Keys.N)
+            {
+                foreach (var light in lights)
+                {
+                    light.IsDiffuseLightEnabled = !light.IsDiffuseLightEnabled;
+                    Debug.WriteLine("State: Ambient: {0}, Diffuse: {1}, Specular: {2} ", light.IsAmbientLightEnabled, light.IsDiffuseLightEnabled, light.IsSpecularLightEnabled);
+                }
+                
+            }
+            else if (e.KeyCode == Keys.M)
+            {
+                foreach (var light in lights)
+                {
+                    light.IsSpecularLightEnabled = !light.IsSpecularLightEnabled; Debug.WriteLine("State: Ambient: {0}, Diffuse: {1}, Specular: {2} ", light.IsAmbientLightEnabled, light.IsDiffuseLightEnabled, light.IsSpecularLightEnabled);
+                }
+                
+            }
+
+
+        }
+
+        private void OpenGlControl_Paint(object sender, PaintEventArgs e)
+        {
+            
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+            Gl.glDepthFunc(Gl.GL_LESS);
+
+            Gl.glPointSize(3);
+            Gl.glPolygonMode(Gl.GL_FRONT, Gl.GL_LINES);
+            Gl.glPolygonMode(Gl.GL_BACK, Gl.GL_LINES);
+
+            Gl.glTranslated(0, 0, -5);
+            
+            rubiksCube.Draw();
+
+            foreach(var light in this.lights)
+            {
+                light.Draw();
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            OpenGlControl.Width = this.Width;
+            OpenGlControl.Height = this.Height;
+            Gl.glViewport(0, 0, this.Width, this.Height);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Glu.gluPerspective(45.0f, (double)this.Width / (double)this.Height, 0.01f, 5000.0f);
+        }
+    }
+}
